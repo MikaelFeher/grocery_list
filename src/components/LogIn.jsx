b4a5d48f1ddab34e215b251886f7f488cb3e8f11
MikaelFeher <mikael.feher@gmail.com>
2017-04-28 15:22:18 +0200

    Adds login feature connected to firebase also adds bootstrap for styling

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,9 +1,57 @@
 import React, { Component } from 'react';
+import { firebaseApp } from '../firebase';
 
 class LogIn extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+			password: '',
+			error: {
+				message: ''
+			}
+		}
+	}
+
+	loginUser() {
+		const { email, password } = this.state;
+		firebaseApp.auth().signInWithEmailAndPassword(email, password)
+			.catch(error => {
+				console.log('error', error);
+				this.setState({error})
+			})
+		console.log('email: ', email);
+		console.log('password: ', password);
+	}
+
 	render() {
+
 		return(
-			<div>Login</div>
+			<div className="form col-sm-4" style={{margin: '5%'}}>
+				<h2>Login</h2>
+				<div className="form-group">
+					<input
+						className="form-control"
+						type="text"
+						placeholder="Enter your email..."
+						onChange={event => this.setState({email: event.target.value})}
+					/>
+					<input
+						className="form-control"
+						type="password"
+						placeholder="...and you password"
+						onChange={event => this.setState({password: event.target.value})}
+					/>
+					<button
+						className="btn btn-primary"
+						onClick={() => this.loginUser()}
+					>
+						Log In
+					</button>
+					<div>{this.state.error.message}</div>
+
+				</div>
+			</div>
 		)
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
+import { firebaseApp } from './firebase';
+import './index.css'
 
 import App from './components/App';
 import LogIn from './components/LogIn';
+
+firebaseApp.auth().onAuthStateChanged(user => {
+	if (user) {
+		const { email } = user;
+		browserHistory.push('/app');
+	} else {
+		browserHistory.replace('/login');
+	}
+})
 
 ReactDOM.render(
 	<Router path='/' history={browserHistory}>
